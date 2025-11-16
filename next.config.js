92f7824fd7a5033163e46b474d37ac7aff6ed4fd
Çağrı Tekin <tekincagri16@gmail.com>
2025-11-16 23:58:20 +0300

    deploy vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    JSONBIN_ID: process.env.JSONBIN_ID,
-    JSONBIN_API_KEY: process.env.JSONBIN_API_KEY,
-  },
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
